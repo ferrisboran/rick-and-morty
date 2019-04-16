2c6ba32f62dd1330aebf6a426b756b6ba7caa2ac
Edgar Ong <edgar@minus.com>
2019-04-16 10:35:24 -0400

    Create new user in database.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,16 @@
 require_relative '../config/environment'
+# require 'rest-client'
+# require 'json'
+
+# MVP
+# - create a new user in database - done
+# - save planet or alien data to database and associate it with user
+# - view user's database
+
+puts "Intro message. What's your username?"
+username = gets.chomp
+User.find_or_create_by(name: username)
 
 
 
-puts "HELLO WORLD"
+# puts "HELLO WORLD"
