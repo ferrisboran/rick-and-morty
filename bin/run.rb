3773c86b602cd63c6dbfac7e0e2d2ed19de0cf15
ferrisboran <ferris@ferryrules.com>
2019-04-16 16:30:05 -0400

    associate aliens with user

--- bin/run.rb
+++ bin/run.rb
@@ -14,13 +14,13 @@ puts "Intro message. What's your username?"
 username = gets.chomp
 
 # LOGIN
-current_user = User.find_or_create_by(name: username)
-Mortydex.find_or_create_by(user_id: current_user.id)
+@current_user = User.find_or_create_by(name: username)
+Mortydex.find_or_create_by(user_id: @current_user.id)
 
 # PLANETS
 planets = Rickmorty::Location.new
 planets_db = planets.all.each do |planet|
-  Planet.create(name: planet["name"])
+  Planet.find_or_create_by(name: planet["name"])
   # binding.pry
 end
 
@@ -36,7 +36,7 @@ aliens_db = aliens.all.each do |character|
   points = character["name"].length
 
   if !!home_id
-    Alien.create(name: name.nil? ? "No name" : name, status: status.nil? ? "Status unknown" : status, species: species.nil? ? "Species not identified" : species, planet_id: character["location"].length == 0 ? @random_planet.id : home_id.id, points: points.nil? ? 0 : points)
+    Alien.find_or_create_by(name: name.nil? ? "No name" : name, status: status.nil? ? "Status unknown" : status, species: species.nil? ? "Species not identified" : species, planet_id: character["location"].length == 0 ? @random_planet.id : home_id.id, points: points.nil? ? 0 : points)
   end
   # if !!home_id
   #   Alien.find_or_create_by(name: name.nil? ? "No name" : name, status: status.nil? ? "Status unknown" : status, species: species.nil? ? "Species not identified" : species, planet_id: home_id.nil? ? "No home" : home_id.id, points: points.nil? ? 0 : points)
@@ -46,9 +46,7 @@ end
 
 ######menu
 
-
-
-  alien = Alien.all.where("planet_id = ?", @random_planet.id)
+alien = Alien.all.where("planet_id = ?", @random_planet.id)
 
 puts "chose: 1.selecte planet /n
             2.Go to a planet/n
@@ -56,15 +54,36 @@ puts "chose: 1.selecte planet /n
             4.Go home/n
             5.View high scores"
 
-
 user_input = gets.chomp
 
-  case user_input
-  when "1"
-    puts @random_planet.name
-    puts alien.empty? ? "no aliens" : alien.sample.name
-    binding.pry
+def save_alien
+  puts "Save alien? yes/no "
+  while yn = gets.chomp
+    case yn.downcase
+    when "yes"
+      puts "Enter name: "
+      name = gets.chomp
+      puts "Species? "
+      species = gets.chomp
+      @current_user.aliens << Alien.find_or_create_by(name: name, status: "Alive", species: species, planet_id: @random_planet.id, points: name.length)
+      break
+    when "no"
+      puts "Fine! Whatever!"
+      break
+    else
+      puts "YES or NO! It's not that hard!"
+    end
   end
+end
+
+case user_input
+when "1"
+  puts @random_planet.name
+  puts alien.empty? ? save_alien : alien.sample.name
+  # binding.pry
+end
 
 
-# binding.pry
+
+
+binding.pry
